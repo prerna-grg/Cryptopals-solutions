4f3cb2263b0ccad22114e3cf103fccecdcb86f71
Prerna Garg <33514735+prerna-grg@users.noreply.github.com>
2018-11-06 17:42:39 +0530

    updated function name & return type

--- set2/1.py
+++ set2/1.py
@@ -1,12 +1,9 @@
 
-from binascii import hexlify, unhexlify
-
-
-def pad(text , block_size ):
+def PKCS7_padding(text , block_size):
 	text_length = len(text)
 	amount_to_pad = block_size - (text_length % block_size)
 	if amount_to_pad == 0:
 		amount_to_pad = block_size
 	return text + (chr(amount_to_pad) * amount_to_pad).encode('ASCII')
 
-print( pad('YELLOW SUBMARINE'.encode('ASCII') , 20 ))
+print( PKCS7_padding('YELLOW SUBMARINE'.encode('ASCII') , 20 ))
